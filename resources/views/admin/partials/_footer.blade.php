427d760c42c85fc80b46c4236c22920bfce070fd
muhamad yusuf <ucuppida207@gmail.com>
2024-02-26 16:50:00 +0700

    first init  commit

---
+++ resources/views/admin/partials/_footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="footer text-center text-muted">
+    All Rights Reserved by SMK Al Hafidz Leuwiliang. Designed and Developed by <a href="https://www.instagram.com/badruu?igsh=OTk0YzhjMDVlZA==" target="_blank">Irfan Badru Tamam</a>.
+</footer>
